183d341504d3282167625615cdec96ce9525b19f
Niall McKenna <mckenna.niall@gmail.com>
2024-01-14 20:44:16 +0000

    Fixed date bug in helpers.js

--- pages/post.js
+++ pages/post.js
@@ -75,10 +75,8 @@ export async function getStaticProps() {
       articleDataArr,
       articleContentArr,
     },
-    // Next.js will attempt to re-generate the page:
-    // - When a request comes in
-    // - At most once every 10 seconds
-    revalidate: 10, // In seconds
+    
+    revalidate: 60 * 60 * 24, // 1 day in seconds
   }
 }
 
